b4eb7a330919861f85cfde99d4cbc8021e9c0768
bakgat <karl.vaniseghem@klimtoren.be>
2015-12-13 20:19:42 +0100

    favico

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,24 +6,31 @@
     <title data-ng-bind="title">noTos</title>
 
     <meta name="keywords" content="links voor de basisschool">
-    <meta name="description" content="noTos is een project om links gekoppeld aan wiskunde doelen te verzamelen. Dit is een project van scholengemeenschap de 4 winden.">
+    <meta name="description"
+          content="noTos is een project om links gekoppeld aan wiskunde doelen te verzamelen. Dit is een project van scholengemeenschap de 4 winden.">
     <meta name="author" content="Karl Van Iseghem | de 4 winden">
     <meta class="viewport" name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="img/favicon.ico">
-
+    @include('common.favicon')
     @include('common.stylesheets')
 </head>
 
 
 <body class="fixed-header" ng-controller="ShellController as shell">
 <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-71228516-1', 'auto');
     ga('send', 'pageview');
@@ -35,7 +42,7 @@
 
         @include('common.header')
 
-        <div ui-view="main" ></div>
+        <div ui-view="main"></div>
 
     </div>
     <!-- .page-box-content -->
